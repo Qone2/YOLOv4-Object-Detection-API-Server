2a4d0c9213f1449dc2d2a7b15b4c03384cabf325
qone2 <whrbdnjs33@naver.com>
2021-11-20 13:52:02 +0900

    Remove unnecessary packages

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ import tensorflow as tf
 physical_devices = tf.config.experimental.list_physical_devices('GPU')
 if len(physical_devices) > 0:
     tf.config.experimental.set_memory_growth(physical_devices[0], True)
-from absl import app, flags, logging
-from absl.flags import FLAGS
 import core.utils as utils
 from core.config import cfg
 from core.yolov4 import filter_boxes
