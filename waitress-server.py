ca339b3e102cca32723bcdd295d42365287b3785
qone2 <whrbdnjs33@naver.com>
2021-11-20 13:53:13 +0900

    Add example option

--- waitress-server.py
+++ waitress-server.py
@@ -5,4 +5,5 @@ import app
 if __name__ == "__main__":
     # serve(app.app, host="0.0.0.0", port=5050)
     # serve(app.app, port=5050)
+    # serve(TransLogger(app.app, setup_console_handler=False), host="0.0.0.0", port=5050)
     serve(TransLogger(app.app, setup_console_handler=False), port=5050)
